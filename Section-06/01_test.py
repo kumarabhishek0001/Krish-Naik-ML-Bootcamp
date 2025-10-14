5afaab20dda08ea36f8debbcc12d9e0491ff5080
Abhishek.dev <157294607+kumarabhishek0001@users.noreply.github.com>
2025-10-15 01:44:41 +0530

    Lecture-1(Importing Libraries and Modules) Completed

---
+++ Section-06/01_test.py
@@ -0,0 +1,10 @@
+from packages.math00 import *
+
+
+
+print("addition: ",addition(3,6))
+print("Substraction: ",substraction(4,2))
+
+from packages.subpackages.mult import *
+
+print("multiplication: ",multiply(2,8))
---
+++ Section-06/packages/__init__.py
@@ -0,0 +1,4 @@
+"""
+This is a speceial file used to define packages and
+initialize namespace
+"""
